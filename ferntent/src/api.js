5952aec5f53e4eaa028243ad7453aebcc8a9e9be
dhyeymovaliya12122-sudo <dhyeymovaliya12122@gmail.com>
2026-06-25 21:26:39 +0530

    updating backend

--- ferntent/src/api.js
+++ ferntent/src/api.js
@@ -1,6 +1,5 @@
 // Auto-detects whether we're running locally or in production
 var API = window.location.hostname === 'localhost'
   ? 'http://localhost:5500'
-  : 'https://srv-d8hgj0d7vvec73du815g.onrender.com';
-
+  : 'https://my-web-89so.onrender.com';
 export default API;
